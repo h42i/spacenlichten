cec2da1d54512738a71c2cc273b2e7501fc8c197
danb <daniel@localhost.localdomain>
2014-02-02 20:44:48 +0100

    Fix.

--- hasi_base.py
+++ hasi_base.py
@@ -30,16 +30,16 @@ def bulb_handler(json, index):
 def bulb_handler_0(json):
     return bulb_handler(json, 0)
 
-def bulb_handler_0(json):
+def bulb_handler_1(json):
     return bulb_handler(json, 1)
 
-def bulb_handler_0(json):
+def bulb_handler_2(json):
     return bulb_handler(json, 2)
 
-def bulb_handler_0(json):
+def bulb_handler_3(json):
     return bulb_handler(json, 3)
 
-bulb_node = node.Node("wlp3s0", aliasing.IP)
+bulb_node = node.Node("eth0", aliasing.IP)
 
 bulb_node.register_device("10.23.42.201", 8, bulb_handler_0)
 bulb_node.register_device("10.23.42.202", 8, bulb_handler_1)
